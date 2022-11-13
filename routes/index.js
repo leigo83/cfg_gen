fbe1a4e0210d5265d2d31e317489f03b61854339
Lei Feng <leigo@fb.com>
2022-11-12 22:55:37 -0800

    fix missing app definition

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 /* GET home page. */
 app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
