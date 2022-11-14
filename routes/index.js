b83ff9a3f7eb7ce3c05ba1d9a41ad3ee5bf346c3
Lei Feng <leigo@fb.com>
2022-11-14 09:46:25 -0800

    fix run time bug

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+const port = 3000;
 /* GET home page. */
 app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.send("Express example");
 });
 
 app.listen(process.env.PORT || port, ()=> console.log("example"));
