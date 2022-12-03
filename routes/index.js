db3f151f5880477183181bdff15c13aaacaedb5c
Lei Feng <leigo@fb.com>
2022-12-03 11:11:56 -0800

    update cfg_gen with first page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 var app = express();
 var path = require('path')
@@ -10,10 +11,30 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', '.hbs');
 
 /* GET home page. */
-app.get('/', function(req, res, next) {
+/*app.get('/', function(req, res, next) {
   console.log(__dirname)
   res.render('index.hbs', { title: 'Express' });
   //res.send("Express example");
+});
+
+app.listen(process.env.PORT || port, ()=> console.log("example"));*/
+
+var cfgDataOrigin = [];
+var cfgData = [];
+
+var listpath = '/Users/leigo/fbsource/fbcode/infra_asic_fpga/ip/xcoder2_0/main/sam/cmodel/src/apps/av1e/cfg_gen/cfgList.json'; //'/../cfgList.json'
+var csvpath = '/../cfgList.csv'
+
+/* GET home page. */
+var rawdata;
+app.get('/', function(req, res, next) {
+  if (cfgDataOrigin.length == 0) {
+     rawdata = fs.readFileSync(listpath);
+     if (rawdata != null) cfgData = JSON.parse(rawdata);
+     cfgDataOrigin = cfgData;
+  }
+  res.render('main', { title: 'Cfg List',
+                        cfgData: cfgData});
 });
 
 app.listen(process.env.PORT || port, ()=> console.log("example"));
