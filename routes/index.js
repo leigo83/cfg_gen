a47991896a6ef9ec61b8aaf8e5630711ae44c273
Lei Feng <leigo@fb.com>
2022-12-02 21:15:18 -0800

    update to test html

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var app = express();
+var path = require('path')
 const port = 3000;
+var hbs = require('express-handlebars');
+
+app.engine('hbs', hbs.engine({defaultLayout: null, extname: '.hbs'}));
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', '.hbs');
+
 /* GET home page. */
 app.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.send("Express example");
+  console.log(__dirname)
+  res.render('index.hbs', { title: 'Express' });
+  //res.send("Express example");
 });
 
 app.listen(process.env.PORT || port, ()=> console.log("example"));
